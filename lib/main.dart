ece6b99285900e76b0ea9d3644412a4709e32723
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-09-27 12:54:06 -0300

    Started with login screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,105 +7,200 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Login(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Container(
+                height: size.height / 2,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color.fromRGBO(102, 14, 218, 1.0),
+                      Color.fromRGBO(234, 86, 253, 1.0),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: size.height / 2,
+                color: Color.fromRGBO(245, 245, 245, 1.0),
+              ),
+            ],
+          ),
+          Center(
+            child: Container(
+              width: size.width - 70.0,
+              height: size.height - 420.0,
+              padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 10.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[400],
+                    blurRadius: 4.0,
+                    offset: Offset(0, 3.0),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: Color.fromRGBO(155, 155, 155, 1.0),
+                        ),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(155, 155, 155, 1.0),
+                            width: 1.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 65.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          color: Color.fromRGBO(155, 155, 155, 1.0),
+                        ),
+                        enabledBorder: const UnderlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: Color.fromRGBO(155, 155, 155, 1.0),
+                            width: 1.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 60.0),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 60.0,
+                        vertical: 8.0,
+                      ),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.centerRight,
+                          end: Alignment.centerLeft,
+                          colors: [
+                            Color.fromRGBO(234, 86, 253, 1.0),
+                            Color.fromRGBO(252, 190, 115, 1.0),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white, fontSize: 18.0),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Don't have an account?",
+                          style: TextStyle(
+                            fontSize: 12.5,
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          " Press here to Sign Up",
+                          style: TextStyle(
+                              fontSize: 12.5,
+                              color: Colors.redAccent,
+                              fontWeight: FontWeight.w700),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 35.0,
+            child: Container(
+              height: 208.0,
+              width: size.width - 70.0,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 2,
+                          width: (size.width - 102.0) / 2,
+                          color: Colors.grey[300],
+                          margin: EdgeInsets.only(right: 5.0),
+                        ),
+                        Text(
+                          "OR",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey[300],
+                          ),
+                        ),
+                        Container(
+                          height: 2,
+                          width: (size.width - 102.0) / 2,
+                          color: Colors.grey[300],
+                          margin: EdgeInsets.only(left: 5.0),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 20.0),
+                      child: Text(
+                        "Login with Touch ID",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.grey[400],
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.fingerprint,
+                      size: 60.0,
+                      color: Color.fromRGBO(234, 86, 253, 0.5),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
